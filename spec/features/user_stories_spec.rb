19bd9a361fe219100e160fc122587e1a0be0f783
lukew244 <luke.webster86@gmail.com>
2016-07-12 12:01:07 +0100

    Adds top-up feature

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -5,9 +5,17 @@ describe 'user stories' do
   # In order to use public transport
   # As a customer
   # I want money on my card
-  it 'so customers can use the card, put money on the card' do
+  it 'so customers can use the card, have a starting balance' do
     card = Oystercard.new
     expect { card.balance }.not_to raise_error
+  end
+
+  # In order to keep using public transport
+  # As a customer
+  # I want to add money to my card
+  it 'allows customer to add money to a card' do
+    card = Oystercard.new
+    expect { card.top_up(10) }.not_to raise_error
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,20 @@
 require 'oystercard'
 
 describe Oystercard do
-  subject(:oystercard) {described_class.new}
+  subject(:card) {described_class.new}
 
-  it 'has money on the card' do
-    expect(oystercard).to respond_to(:balance)
+  describe "#balance" do
+    it 'has a starting value of 0' do
+      expect(card.balance).to eq(0)
+    end
   end
+
+  describe "#top_up" do
+    it 'can be topped up' do
+      expect{ card.top_up(5) }.to change{ card.balance }.by 5
+      expect{ card.top_up(1) }.to eq(6)
+    end
+  end
+
+
 end
